ae28c467b9a2786e12f2372eeba4cef2530ac2b9
Jérôme Abel <contact@jeromeabel.net>
2023-01-31 11:52:03 +0100

    add Score component fix #14

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,14 +1,46 @@
+import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts';
 import styles from './Score.module.scss';
 
 const Score = ({ value }) => {
+  const data = [{ score: value }];
+  const startAngle = 90;
+  const endAngle = startAngle + value * -360;
   return (
     <div className={styles.container}>
-      <span>Score</span>
+      <div className={styles.chart}>
+        <h2>Score</h2>
+        <div className={styles.chart__legend}>
+          <h3>{value * 100}%</h3>
+          <p>
+            de votre
+            <br />
+            objectif
+          </p>
+        </div>
 
-      <div className={styles.score}>
-        <span>{value * 100} % </span>
-        <span>de votre</span>
-        <span>objectif</span>
+        <ResponsiveContainer width="100%" height="100%">
+          <RadialBarChart
+            innerRadius="85%"
+            outerRadius="100%"
+            data={data}
+            startAngle={startAngle}
+            endAngle={endAngle}
+            margin={{
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 0,
+            }}
+            className={styles.chart__shape}
+          >
+            <RadialBar
+              dataKey="score"
+              fill="var(--color-primary)"
+              stroke-linejoin="round"
+              cornerRadius={100}
+            />
+          </RadialBarChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
